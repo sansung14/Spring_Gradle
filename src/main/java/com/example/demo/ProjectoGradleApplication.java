aac87b827fa646dca6ee0b9842aa3b7b78b35a64
sansung14 <78426816+sansung14@users.noreply.github.com>
2021-03-08 19:18:27 +0100

    Spring_Gradle

---
+++ src/main/java/com/example/demo/ProjectoGradleApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoGradleApplication.class, args);
+
+	}
+
+
+	
+}
